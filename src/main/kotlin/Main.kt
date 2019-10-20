919d4173edd2222dd823aefa2d93c4af35d40d62
jiadibo <jihad.strange@gmail.com>
2019-10-20 23:56:13 +0800

    add gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
 import io.javalin.Javalin
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.*
 
-fun main(args: Array<String>){
-    val app = Javalin.create().start(8000)
-    app.get("/"){ ctx -> ctx.result("Hello world!")}
+fun main(args: Array<String>) {
+
+    Database.connect("jdbc:sqlite:/data/data.db", "org.sqlite.JDBC")
+
+    transaction {
+        addLogger(StdOutSqlLogger)
+
+    }
+
 }
