e135dc385eeee3a99ffe24b1799d4aa57d76fab6
jiadibo <jihad.strange@gmail.com>
2019-10-21 09:42:16 +0800

    pull

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,28 @@
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.lang.Exception
 
-fun main(args: Array<String>){
-    val app = Javalin.create().start(8000)
-    app.get("/"){ ctx -> ctx.result("Hello world!")}
+fun main(args: Array<String>) {
+
+
+    val userDao = UserDao()
+
+    val app = Javalin.create().apply {
+        exception(Exception::class.java){ e, ctx -> e.printStackTrace()}
+        error(404){ctx->ctx.json("not found")}
+    }.start(8000)
+
+    Database.Companion.connect("jdbc:sqlite:/data/data.db", "org.sqlite.JDBC")
+
+    app.routes {
+        get("/users"){ ctx ->
+            ctx.json(userDao.users)
+        }
+        
+    }
+
+
 }
